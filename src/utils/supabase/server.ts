d37a0d8d49de9e839dfbc499eb5dc66c367ecb9a
daichi_sato <daichiexperience@gmail.com>
2024-08-05 22:53:12 +0900

    first commit

---
+++ src/utils/supabase/server.ts
@@ -0,0 +1,32 @@
+import { type CookieOptions, createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+export const createClient = () => {
+	const cookieStore = cookies();
+
+	return createServerClient(
+		process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "",
+		{
+			cookies: {
+				getAll() {
+					return cookieStore.getAll();
+				},
+				setAll(cookiesToSet) {
+					try {
+						for (const { name, value, options } of cookiesToSet) {
+							cookieStore.set(name, value, options);
+						}
+					} catch {
+						// The `setAll` method was called from a Server Component.
+						// This can be ignored if you have middleware refreshing
+						// user sessions.
+						console.warn(
+							"The `setAll` method was called from a Server Component. This can be ignored if you have middleware refreshing user sessions.",
+						);
+					}
+				},
+			},
+		},
+	);
+};
